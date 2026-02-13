7ab0e4dd2b490e880c915d5e9337f3e366420ee3
Benedikt Strehle <benedikt@unyt.org>
2026-02-14 00:11:56 +0100

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-use datex::core::runtime::{Runtime, RuntimeConfig, RuntimeRunner};
+use std::time::Duration;
+use tokio::time::sleep;
+use datex::core::runtime::{Runtime};
 
-#[datex::main]
+#[datex::main("../example-config-server.dx")]
 async fn main(runtime: Runtime) {
-    println!("Hello, world!");
+    flexi_logger::init();
+    println!("Hello, DATEX - Version: {}", runtime.version);
+
+    loop {
+        // print com hub connection information every 5 seconds
+        runtime.com_hub().print_metadata();
+        sleep(Duration::from_secs(5)).await;
+    }
 }
