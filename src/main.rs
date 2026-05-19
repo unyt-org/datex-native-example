a936204e04e68d947eea8af58aaaf9fde9d1602e
Benedikt Strehle <benedikt@unyt.org>
2026-05-19 23:16:55 +0200

    remove logger init

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,8 @@ use datex::core::runtime::{Runtime};
 
 #[datex::main("../config.dx")]
 async fn main(runtime: Runtime) {
-    flexi_logger::init();
     println!("Hello, DATEX - Version: {}", runtime.version());
-
+    
     loop {
         // print com hub connection information every 5 seconds
         runtime.com_hub().print_metadata();
