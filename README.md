cbfca3742eae7b41d4c1373d1eea92996d433ca5
Jonas Strehle <jonas@strehles.info>
2026-06-28 11:28:57 +0200

    cleanup

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 This is an example project that demonstrates how to use the DATEX runtime in a native Rust application.
 You can run this example with `cargo run`.
 To specify a log level, use the `RUST_LOG` environment variable, e.g. `RUST_LOG=info cargo run` - by default, the log level is set to `warn`.
+
+---
+
+<sub>&copy; unyt 2026 • [unyt.org](https://unyt.org)</sub>
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use std::time::Duration;
 use tokio::time::sleep;
-use datex::core::runtime::{Runtime};
 
 #[datex::main("../config.dx")]
 async fn main(runtime: Runtime) {
-    println!("Hello, DATEX - Version: {}", runtime.version());
-    
+    println!("Hello from DATEX! (version: {})", runtime.version());
+
+    // print com hub connection information every 5 seconds
     loop {
-        // print com hub connection information every 5 seconds
-        runtime.com_hub().print_metadata();
+        println!("ComHub Metadata: {:#?}", runtime.com_hub().get_metadata());
         sleep(Duration::from_secs(5)).await;
     }
 }
